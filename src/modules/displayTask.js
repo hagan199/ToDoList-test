e66b9ea34978c1d3586e807eec232b32c93cf644
hagan23 <haganemmanuel23@gmail.com>
2023-05-18 10:54:51 +0000

    update

--- src/modules/displayTask.js
+++ src/modules/displayTask.js
@@ -2,77 +2,51 @@ export const displayTask = () => {
   const mytaskList = document.querySelector('.myTasksList');
   const taskArray = JSON.parse(localStorage.getItem('taskArray')) || [];
   taskArray.sort((a, b) => a.index - b.index);
-  
-  const fragment = document.createDocumentFragment();
-  
-  for (const task of taskArray) {
-    const taskElement = document.createElement('div');
-    taskElement.classList.add('addTask');
-    
-    const checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    checkbox.classList.add('Input-checkbox');
-    
-    const inputText = document.createElement('input');
-    inputText.type = 'text';
-    inputText.classList.add('input-text');
-    inputText.value = task.description;
-    
-    const deleteIcon = document.createElement('span');
-    deleteIcon.classList.add('delete-task-icon');
-    deleteIcon.textContent = '\u{1F5D1}';
-    
-    taskElement.appendChild(checkbox);
-    taskElement.appendChild(inputText);
-    taskElement.appendChild(deleteIcon);
-    
-    fragment.appendChild(taskElement);
-  }
-  
   mytaskList.innerHTML = '';
-  mytaskList.appendChild(fragment);
+  for (let i = 0; i < taskArray.length; i += 1) {
+    const html = `
+      <div class="addTask">
+        <input type="checkbox" class="Input-checkbox">
+        <input type="text" class="input-text" value="${taskArray[i].description}">
+        <span class="delete-task-icon">&#x1F5D1;</span>
+      </div>
+    `;
+    mytaskList.innerHTML += html;
+  }
 };
 
+// ADD A NEW TASK
 export const addTasks = (addTask) => {
   const addTaskInput = document.querySelector('.addInput');
-  
   if (addTask !== '') {
     const taskArray = JSON.parse(localStorage.getItem('taskArray')) || [];
     taskArray.push({ completed: false, description: addTask });
-    
     for (let i = 1; i <= taskArray.length; i += 1) {
       taskArray[i - 1].index = i;
     }
-    
     localStorage.setItem('taskArray', JSON.stringify(taskArray));
     addTaskInput.value = '';
-    displayTask();
+    displayTask(); // added to display new task
   }
 };
 
+// DELETE A TASK
 export const deleteTask = (index) => {
   const taskArray = JSON.parse(localStorage.getItem('taskArray')) || [];
   taskArray.splice(index, 1);
-  
   for (let i = 1; i <= taskArray.length; i += 1) {
     taskArray[i - 1].index = i;
   }
-  
   localStorage.setItem('taskArray', JSON.stringify(taskArray));
   displayTask();
 };
 
+// editTask A TASK
 export const editTask = (index) => {
   const taskArray = JSON.parse(localStorage.getItem('taskArray')) || [];
   const textInputs = document.querySelectorAll('.input-text');
-  
-  const handleInputChange = (event) => {
-    taskArray[index].description = event.target.value;
+  textInputs[index].addEventListener('change', () => {
+    taskArray[index].description = textInputs[index].value;
     localStorage.setItem('taskArray', JSON.stringify(taskArray));
-  };
-
-  textInputs[index].addEventListener('change', handleInputChange);
+  });
 };
-
-// Call the displayTask function when the page loads to show the tasks
-window.addEventListener('DOMContentLoaded', displayTask);
